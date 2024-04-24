64d6b15ed43a7f93802e1b13dbf4a1081b4f1e3a
FreeBuu <freebuu.dev@gmail.com>
2024-04-24 20:41:40 +0300

    one more test

--- tests/Unit/FilterParamTest.php
+++ tests/Unit/FilterParamTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit;
 
 use FreeBuu\LaravelFilterable\Params\FilterCaseEnum;
 use FreeBuu\LaravelFilterable\Params\FilterParam;
+use Illuminate\Database\Eloquent\Builder;
 use Tests\TestCase;
 
 class FilterParamTest extends TestCase
@@ -30,7 +31,51 @@ class FilterParamTest extends TestCase
     /** @dataProvider provideTestNormalizeValues */
     public function testNormalizeValues(FilterCaseEnum $case, mixed $value, mixed $expected): void
     {
-        $param = new FilterParam($case, 'some-field', $value);
-        $this->assertEquals($expected, $param->normalizedValue());
+        $filter = new FilterParam($case, 'some-field', $value);
+        $this->assertEquals($expected, $filter->normalizedValue());
+    }
+
+    public function provideTestApply(): array
+    {
+        return [
+            [FilterCaseEnum::FROM, 'where', '>='],
+            [FilterCaseEnum::TO, 'where', '<='],
+            [FilterCaseEnum::SORT, 'sort'],
+            [FilterCaseEnum::SEARCH, 'where', FilterParam::$likeFunction],
+            [FilterCaseEnum::START_WITH, 'where', FilterParam::$likeFunction],
+            [FilterCaseEnum::WHERE, 'whereIn'],
+        ];
+    }
+
+    /** @dataProvider provideTestApply */
+    public function testApply(FilterCaseEnum $case, string $method, string $operator = null)
+    {
+        $filter = new FilterParam($case, 'some-field', 'some_value');
+        $builder = \Mockery::mock(Builder::class);
+        $builder
+            ->shouldReceive($method)
+            ->withArgs(array_values(array_filter([$filter->field, $operator, $filter->normalizedValue()])));
+        dump(array_filter([$filter->field, $operator, $filter->normalizedValue()]));
+        $filter->apply($builder);
+        $this->addToAssertionCount(1);
+    }
+
+    public function testApplyFilter()
+    {
+        $builder = \Mockery::mock(Builder::class);
+        $builder->shouldReceive('where')->with('some_value', 'some_field_value');
+
+        $callback = function ($builder, $value, $filedValue) {
+            $builder->where($value, $filedValue);
+        };
+
+        $filter = new FilterParam(
+            FilterCaseEnum::FILTER,
+            $callback,
+            'some_value',
+            'some_field_value'
+        );
+        $filter->apply($builder);
+        $this->addToAssertionCount(1);
     }
 }
